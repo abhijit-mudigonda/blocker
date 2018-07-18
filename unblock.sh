d0718a1c0730ba99c89bd791bfb2364fda05748b
Abhijit Mudigonda <abhijit.mudigonda@gmail.com>
2018-07-18 18:58:53 -0400

    added time lag on unblock

--- unblock.sh
+++ unblock.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+echo "If you're trying to unblock something that isn't mail or messenger, you shouldn't use this \ 
+turn back now"
+sleep 30
+echo "Smh are you really sure you were being so productive"
+sleep 5
+
 if grep -q "$1" /etc/hosts
 then
     sudo sed -i "/$1/d" /etc/hosts
--- unblock_list.sh
+++ unblock_list.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 
+echo "You probably don't deserve this - just unblock gmail and messenger manually"
+sleep 2m
+echo "Are you sure you want this?"
+sleep 30
+
 cat ~/.block_list.txt | while read url
 do
-    unblock $url
+    sudo sed -i "/$url/d" /etc/hosts
 done
